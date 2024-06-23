739fb6eac5be9bd9a77cdf39ac7117d99f895974
Ruslan_Mendybayev <ruslan_mendybayev@epam.com>
2024-06-22 23:03:55 -0400

    fix: adjusted structure of TableContract

--- ingestion_lib/utils/data_contract.py
+++ ingestion_lib/utils/data_contract.py
@@ -4,9 +4,8 @@ from pydantic import BaseModel
 
 
 class TableContract(BaseModel):
-    db_name: str
     table_name: str
-    schema_: str
+    schema_name: str
     batch_timestamp: str
     watermark_columns: List[str]
     lower_bound: Optional[str] = None
@@ -14,11 +13,8 @@ class TableContract(BaseModel):
     full_load: bool = False
     load_type: str = 'incremental'
     target_schema: str
-    mount_point: str
 
     class Config:
         anystr_lower = True
         allow_population_by_field_name = True
-        fields = {
-            'schema_': {'alias': 'schema'}
-        }
+
